6cee2e171587dbefd00c505662dee14c875ec10e
NebyX1 <blackcat_neme@hotmail.com>
2023-04-27 00:22:47 -0300

    second commit

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3500)
+    # app.run(debug=True, port=3500) esto solo para cuando se esté corriendo en modo de prueba
+    app.run()
